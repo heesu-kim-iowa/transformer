fa93c2286205d87487bdfafd75c35d2a3ebd45ad
hkim135 <heesu-kim@uiowa.edu>
2020-11-10 10:19:35 -0600

    have followed up by Feed Forward

--- tutorial.py
+++ tutorial.py
@@ -1,12 +1,221 @@
 import tensorflow_datasets as tfds
-# import tensorflow as tf
-#
-# import time
-# import numpy as np
-# import matplotlib.pyplot as plt
+import tensorflow as tf
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 
+"""Setup input pipeline"""
 examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True, as_supervised=True)
 train_examples, val_examples = examples['train'], examples['validation']
+MAX_EXAMPLES = 100      # Only take 100 examples to reduce computing time. It turned out it doesn't mean that '100 sentences'. It somehow limit the sentences to be loaded but I have no idea about the relation between the number of exmaples and the number of sentences loaded.
+train_examples = train_examples.take(MAX_EXAMPLES)
+val_examples = val_examples.take(MAX_EXAMPLES)
+
+"""Generate a dictionary. (It takes somewhat long time)"""
+# (tokenize: to split a sentence into words and convert the words to their corresponding index.)
+tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+    (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+    (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+"""Check if tokenizer works right"""
+sample_string = 'He is a boy.'
+tokenized_string = tokenizer_en.encode(sample_string)
+print('Tokenized string is {}'.format(tokenized_string))
+original_string = tokenizer_en.decode(tokenized_string)
+print('The original string: {}'.format(original_string))
+assert original_string == sample_string
+# The tokenizer encodes the string by breaking it into subwords if the word is not in its dictionary.
+for ts in tokenized_string:
+    print('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
+
+"""Setup the hyper parameters for training"""
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+"""Add a start and end token to the input and target"""
+def encode(lang1, lang2):
+    lang1 = [tokenizer_pt.vocab_size] + tokenizer_pt.encode(
+      lang1.numpy()) + [tokenizer_pt.vocab_size+1]
+    lang2 = [tokenizer_en.vocab_size] + tokenizer_en.encode(
+      lang2.numpy()) + [tokenizer_en.vocab_size+1]
+    return lang1, lang2
+# Operations inside `.map()` run in graph mode and receive a graph tensor that do not have a numpy attribute. The `tokenizer` expects a string or Unicode symbol to encode it into integers. Hence, you need to run the encoding inside a `tf.py_function`, which receives an eager tensor having a numpy attribute that contains the string value.
+def tf_encode(pt, en):
+    result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
+    result_pt.set_shape([None])
+    result_en.set_shape([None])
+    return result_pt, result_en
+
+"""Note: To keep this example small and relatively fast, drop examples with a length of over 40 tokens."""
+MAX_LENGTH = 40
+def filter_max_length(x, y, max_length=MAX_LENGTH):
+    return tf.logical_and(tf.size(x) <= max_length,
+                        tf.size(y) <= max_length)
+
+"""Prepare dataset"""
+# train dataset
+train_dataset = train_examples.map(tf_encode)
+train_dataset = train_dataset.filter(filter_max_length)
+# cache the dataset to memory to get a speedup while reading from it.
+train_dataset = train_dataset.cache()
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+# Validation dataset
+val_dataset = val_examples.map(tf_encode)
+val_dataset = val_dataset.filter(filter_max_length).padded_batch(BATCH_SIZE)
+
+# 'next' and 'iter' convert the data type Tensor something(?) into intgers so that we can see the real value it has.
+pt_batch, en_batch = next(iter(val_dataset))
+print(pt_batch, en_batch)
+
+"""Positional encoding"""
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+"""Masking"""
+# The one that WILL BE USED is indicated as 0, otherwise 1.
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    # add extra dimensions to add the padding
+    # to the attention logits.
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+# This is a demonstration of how the mask is generated
+# x = tf.constant([[7, 6, 0, 0, 1], [1, 2, 3, 0, 0], [0, 0, 0, 4, 5]])
+# print(create_padding_mask(x))
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+# x = tf.random.uniform((1, 3))
+# temp = create_look_ahead_mask(x.shape[1])
+# print(temp)
+
+"""Scaled dot product attention"""
+def scaled_dot_product_attention(q, k, v, mask):
+    """Calculate the attention weights.
+    q, k, v must have matching leading dimensions.
+    k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+    The mask has different shapes depending on its type(padding or look ahead)
+    but it must be broadcastable for addition.
+
+    Args:
+    q: query shape == (..., seq_len_q, depth)
+    k: key shape == (..., seq_len_k, depth)
+    v: value shape == (..., seq_len_v, depth_v)
+    mask: Float tensor with shape broadcastable
+          to (..., seq_len_q, seq_len_k). Defaults to None.
+
+    Returns:
+    output, attention_weights
+    """
+
+    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    # add up to 1.
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
+
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+
+    return output, attention_weights
+
+def print_out(q, k, v):
+  temp_out, temp_attn = scaled_dot_product_attention(
+      q, k, v, None)
+  print ('Attention weights are:')
+  print (temp_attn)
+  print ('Output is:')
+  print (temp_out)
+
+"""Multi-head attention"""
+# I don't understand this process only by looking
+class MultiHeadAttention(tf.keras.layers.Layer):
+  def __init__(self, d_model, num_heads):
+    super(MultiHeadAttention, self).__init__()
+    self.num_heads = num_heads
+    self.d_model = d_model
+
+    assert d_model % self.num_heads == 0
+
+    self.depth = d_model // self.num_heads
+
+    self.wq = tf.keras.layers.Dense(d_model)
+    self.wk = tf.keras.layers.Dense(d_model)
+    self.wv = tf.keras.layers.Dense(d_model)
+
+    self.dense = tf.keras.layers.Dense(d_model)
+
+  def split_heads(self, x, batch_size):
+    """Split the last dimension into (num_heads, depth).
+    Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+    """
+    x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+    return tf.transpose(x, perm=[0, 2, 1, 3])
+
+  def call(self, v, k, q, mask):
+    batch_size = tf.shape(q)[0]
+
+    q = self.wq(q)  # (batch_size, seq_len, d_model)
+    k = self.wk(k)  # (batch_size, seq_len, d_model)
+    v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+    q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+    k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+    v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+    # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+    # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+    scaled_attention, attention_weights = scaled_dot_product_attention(
+        q, k, v, mask)
+
+    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+
+    concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+
+    output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+    return output, attention_weights
+
+# I don't get why d_model=512 and num_heads=8
+temp_mha = MultiHeadAttention(d_model=512, num_heads=8)
+y = tf.random.uniform((1, 60, 512))  # (batch_size, encoder_sequence, d_model) #(What is encoder_sequence?: "collection of all words from the question"<--?)
+out, attn = temp_mha(y, k=y, q=y, mask=None) #<-- It execute call()
+print(out.shape, attn.shape)
+
+"""Point wise feed forward network"""
+def point_wise_feed_forward_network(d_model, dff):
+    return tf.keras.Sequential([
+      tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+      tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+    ])
+
+sample_ffn = point_wise_feed_forward_network(512, 2048)
+print(sample_ffn(tf.random.uniform((64, 50, 512))).shape)
 
 
-print("chckpt")
+
+print("check point")
